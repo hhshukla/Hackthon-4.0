773890fcd29979c40fb64ec7961934ec687a6cdb
hhshukla <hshukla@horizontal.com>
2025-04-05 17:12:51 +0530

    build error resolve

--- Hackthon-4.0/hackthon-4.0/src/pages/products.tsx
+++ Hackthon-4.0/hackthon-4.0/src/pages/products.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import SearchFilter from '@/Components/SearchFilter/SearchFilter';
-import Header from '@/Components/Header/Header';
-import Footer from '@/Components/Footer/Footer';
-import { ThemeProvider } from 'next-themes';
-import ThemeSwitcher from '@/Components/ThemeSwicher/ThemeSwithcher';
+import React from "react";
+// import SearchFilter from '@/Components/SearchFilter/SearchFilter';
+import Header from "@/Components/Header/Header";
+import Footer from "@/Components/Footer/Footer";
+import { ThemeProvider } from "next-themes";
+import ThemeSwitcher from "@/Components/ThemeSwicher/ThemeSwithcher";
 
 const ProductsPage = () => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <ThemeSwitcher />
       <Header />
-      <SearchFilter />
+      {/* <SearchFilter /> */}
       <Footer />
     </ThemeProvider>
   );
